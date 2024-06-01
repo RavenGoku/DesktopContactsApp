55c660b6120ec73c3481be6d078db79375004616
RavenGoku <raven.goku@gmail.com>
2024-06-01 14:30:08 +0100

    Add project files.

---
+++ NewConctactWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewConctactWindow.xaml
+    /// </summary>
+    public partial class NewConctactWindow : Window
+    {
+        private MainWindow mainWindow;
+
+        public NewConctactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO:save contact
+            //closing window after save new contact
+            this.Close();
+        }
+    }
+}
